eb5991ffa4b5f6a61d72ea9badcaeca2ed65788d
James Nobis <quel@quelrod.net>
2011-01-22 11:20:31 -0600

    update (C) pull forward but comment out checkpoint code from rtperfecter0.0

--- Server Applications/rtperfecter0.1/RTWrite.cpp
+++ Server Applications/rtperfecter0.1/RTWrite.cpp
@@ -1,20 +1,20 @@
 /*
 	Copyright (C) 2008 Steve Thomas <SMT837784@yahoo.com>
 
-	This file is part of RT Perfecter v0.0.
+	This file is part of RT Perfecter v0.1.
 
-	RT Perfecter v0.0 is free software: you can redistribute it and/or modify
+	RT Perfecter v0.1 is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	RT Perfecter v0.0 is distributed in the hope that it will be useful,
+	RT Perfecter v0.1 is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with RT Perfecter v0.0.  If not, see <http://www.gnu.org/licenses/>.
+	along with RT Perfecter v0.1.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
@@ -81,7 +81,14 @@ void RTWrite::writeChain(RTChain *chain)
 	{
 		perror("temp.rt");
 		exit(1);
-	}	
+	}
+/*
+	if (fwrite((void*)&chain->checkpoint, 2, 1, m_pFile) != 1)
+	{
+		perror("temp.rt");
+		exit(1);
+	}
+*/
 	m_curFileChains++;
 	if (m_curFileChains >= m_chainsPerFile)
 	{
